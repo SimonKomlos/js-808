e84c5d0d67cc5026a52cc62f07959182d8d5baf5
s.komlos@gmail.com <s.komlos@gmail.com>
2019-04-30 01:21:12 -0400

    play, pause, stop, and other sequences added

--- drum-machine/src/app/app.component.ts
+++ drum-machine/src/app/app.component.ts
@@ -15,8 +15,13 @@ export interface Sequence {
 })
 export class AppComponent {
 
-  bpm:number=60;
-  beat:number=0;
+  bpm:number=128;
+  beat:number=1;
+
+  timeInterval:any;
+
+  isPlaying:boolean=false;
+  isPaused:boolean=false;
 
   selectedSequence:any;
   sequences: Sequence[] = [
@@ -41,6 +46,50 @@ export class AppComponent {
           steps: [1,1,1,1,0,0,0,1,0,0,1,1,0,0,0,1]
         },
       ]
+    },
+    {
+      name: "Sequence #2",
+      id: 1,
+      instruments: [
+        {
+          name: "Kick",
+          steps: [1,0,0,1,0,1,0,0,1,0,0,1,1,0,0,1]
+        },
+        {
+          name: "Snare",
+          steps: [0,1,0,0,1,1,0,1,0,0,0,1,0,1,0,0]
+        },
+        {
+          name: "Open Hat",
+          steps: [0,0,1,1,0,1,0,1,0,1,0,1,0,1,0,1]
+        },
+        {
+          name: "Crash",
+          steps: [1,0,1,0,0,1,0,1,0,0,1,0,0,1,0,0]
+        },
+        {
+          name: "Cowbell",
+          steps: [0,0,1,0,1,0,0,0,1,0,1,0,1,1,0,1]
+        },
+      ]
+    },
+    {
+      name: "Sequence #3",
+      id: 2,
+      instruments: [
+        {
+          name: "Kick",
+          steps: [1,0,1,0,0,0,0,1,0,0,1,0,0,0,0,0]
+        },
+        {
+          name: "Snare",
+          steps: [0,1,0,0,0,1,0,1,0,0,1,1,0,1,0,0]
+        },
+        {
+          name: "Open Hat",
+          steps: [1,0,1,1,0,0,1,1,0,0,1,0,0,1,0,0]
+        },
+      ]
     }
   ]
 
@@ -49,25 +98,40 @@ export class AppComponent {
   }
 
   setSelectedSequence(val) {
-    console.log(val)
     this.selectedSequence = this.sequences[val].instruments;
-    console.log(this.selectedSequence)
+    this.stop();
   }
 
   play() {
-    let speed = ((60/this.bpm)*4);
-    this.beat = 0;
+    let speed = ((60/this.bpm)*4)/8;
+    this.isPlaying = true;
+    this.isPaused = false;
     let maxBeat = this.selectedSequence[0].steps.length;
-    let timePeriod;
-    clearInterval(timePeriod)
-    timePeriod = setInterval(() => {
+    this.timeInterval = setInterval(() => {
       $('.beat-square').removeClass("active");
       $('.beat-'+this.beat).addClass("active");
       this.beat++;
       if(this.beat > maxBeat) {
-        this.beat = 0;
+        this.beat = 1;
       }
-    }, 100)
+      //I'm not entirely sure how bpm works converted in milliseconds so I'm multiplying
+      //it by 1000 to get the bpm into seconds. This might be wrong. I'm
+      //not confident. I'd have to do more research into time signatures
+    }, speed*1000)
+  }
+
+  pause() {
+    this.isPlaying = false;
+    this.isPaused = true;
+    clearInterval(this.timeInterval)
+  }
+
+  stop() {
+    this.isPlaying = false;
+    this.isPaused = false;
+    $('.beat-square').removeClass("active");
+    clearInterval(this.timeInterval)
+    this.beat = 1;
   }
 
 }
